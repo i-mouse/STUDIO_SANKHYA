df24c69bbaf36dd4c0f740b8e0581c895cb21a40
imouse <48970745+i-mouse@users.noreply.github.com>
2024-10-23 09:31:21 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -857,3 +857,25 @@ console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55')
 })(jQuery);
 
 
+
+// Projects slider code
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+
+
+

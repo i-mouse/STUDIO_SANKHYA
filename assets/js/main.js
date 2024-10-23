a71c88d5d82f131c5b7b45f96956dcdafbefc2d8
imouse <48970745+i-mouse@users.noreply.github.com>
2024-10-23 10:01:05 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -860,22 +860,31 @@ console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55')
 
 // Projects slider code
 
+let slideIndex = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-//Autoplay
-let slideIndex = 0;
-showSlides();
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-function showSlides() {
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  dots[slideIndex-1].className += " active";
 }
-
-
